7cdd121a9d5e17305039804f489bebfd25e9a82c
Andrii Krenevych <akrenevych@gmail.com>
2023-11-21 13:07:14 +0200

    Fragment - Fragment transactions

--- app/src/main/java/com/example/shop/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/shop/presentation/ShopItemActivity.kt
@@ -3,129 +3,47 @@ package com.example.shop.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import androidx.core.widget.addTextChangedListener
-import androidx.lifecycle.ViewModelProvider
 import com.example.shop.R
 import com.example.shop.domain.ShopItem
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 
 class ShopItemActivity : AppCompatActivity() {
-
-//    private lateinit var tilName: TextInputLayout
-//    private lateinit var tilCount: TextInputLayout
-//
-//    private lateinit var editName: TextInputEditText
-//    private lateinit var editCount: TextInputEditText
-//
-//    private lateinit var buttonSave: Button
-//
-//    private lateinit var viewModel: ShopItemViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
-
-//        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
-//
-//        initViews()
-//        parseIntent()
-//        registerLiveData()
-
+        parseIntent()
     }
 
-//    private fun initViews() {
-//        tilName = findViewById(R.id.til_name)
-//        tilCount = findViewById(R.id.til_count)
-//
-//        editName = findViewById(R.id.edit_text_name)
-////        editName.addTextChangedListener(object : TextWatcher {
-////            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-////            }
-////
-////            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-////                tilName.error = null
-////            }
-////
-////            override fun afterTextChanged(p0: Editable?) {
-////            }
-////        })
-//        editName.addTextChangedListener {
-//            tilName.error = null
-//        }
-//
-//        editCount = findViewById(R.id.edit_text_count)
-//        editCount.addTextChangedListener {
-//            tilCount.error = null
-//        }
-//
-//        buttonSave = findViewById(R.id.buttonSave)
-//    }
+    private fun setupFragment(fragment: ShopItemFragment){
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.shop_item_container, fragment)
+            .commit()
+    }
 
-//    private fun lunchActivityForAdd(){
-//        buttonSave.setOnClickListener {
-//            viewModel.addItem(editName.text, editCount.text)
-//        }
-//    }
-//    private fun lunchActivityForEdit(){
-//        viewModel.getItem(itemId)
-//
-//        buttonSave.setOnClickListener {
-//            viewModel.editItem(editName.text, editCount.text)
-//        }
-//    }
+    private var mode: String = MODE_UNDEF
+    private var itemId: Long = ShopItem.UNDEFINED
+    private fun parseIntent(){
+        if (intent.hasExtra(EXTRA_MODE)){
+            mode = intent.getStringExtra(EXTRA_MODE) ?: MODE_UNDEF
+            Log.d(TAG, "parseIntent: mode = $mode")
+            if (mode == MODE_ADD){
+                setupFragment(ShopItemFragment())
+            }
+            else if (mode == MODE_EDIT && intent.hasExtra(EXTRA_ITEM_ID)){
+                itemId = intent.getLongExtra(EXTRA_ITEM_ID, ShopItem.UNDEFINED)
+                Log.d(TAG, "parseIntent: itemId = $itemId")
+                setupFragment(ShopItemFragment())
+            } else {
+                throw IllegalArgumentException("Item Id is not defined")
+            }
+        } else {
+            throw IllegalArgumentException("Mode is not defined")
+        }
+    }
 
-//    private var mode: String = MODE_UNDEF
-//    private var itemId: Long = ShopItem.UNDEFINED
-//    private fun parseIntent(){
-//        if (intent.hasExtra(EXTRA_MODE)){
-//            mode = intent.getStringExtra(EXTRA_MODE) ?: MODE_UNDEF
-//            Log.d(TAG, "parseIntent: mode = $mode")
-//            if (mode == MODE_ADD){
-//                lunchActivityForAdd()
-//            }
-//            else if (mode == MODE_EDIT && intent.hasExtra(EXTRA_ITEM_ID)){
-//                itemId = intent.getLongExtra(EXTRA_ITEM_ID, ShopItem.UNDEFINED)
-//                Log.d(TAG, "parseIntent: itemId = $itemId")
-//                lunchActivityForEdit()
-//            } else {
-//                throw IllegalArgumentException("Item Id is not defined")
-//            }
-//        } else {
-//            throw IllegalArgumentException("Mode is not defined")
-//        }
-//    }
-//
-//    private fun registerLiveData(){
-//        viewModel.itemLiveData.observe(this){
-//            editName.setText(it.name)
-//            editCount.setText(it.count.toString())
-//        }
-//
-//        viewModel.errorNameLD.observe(this){
-//            tilName.error = if (it){
-//                null
-//            } else {
-//                getString(R.string.name_error)
-//            }
-//        }
-//        viewModel.errorCountLD.observe(this){
-//            tilCount.error = if (it){
-//                null
-//            } else {
-//                getString(R.string.count_error)
-//            }
-//        }
-//
-//        viewModel.finishActivityLD.observe(this){
-////            finish()
-//            onBackPressed()
-//        }
-//    }
 
     companion object {
-
-        val TAG = "XXXXXX"
+        const val TAG = "XXXXXX"
     }
 }
--- app/src/main/java/com/example/shop/presentation/ShopItemFragment.kt
+++ app/src/main/java/com/example/shop/presentation/ShopItemFragment.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.shop.R
+import com.example.shop.domain.ShopItem
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class ShopItemFragment : Fragment() {
 
@@ -15,6 +21,111 @@ class ShopItemFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_shop_item, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews(view)
+    }
+
+    private lateinit var tilName: TextInputLayout
+    private lateinit var tilCount: TextInputLayout
+
+    private lateinit var editName: TextInputEditText
+    private lateinit var editCount: TextInputEditText
+
+    private lateinit var buttonSave: Button
+
+    private lateinit var viewModel: ShopItemViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+
+//        parseIntent()
+        registerLiveData()
+
+    }
+
+    private fun initViews(view: View) {
+        tilName = view.findViewById(R.id.til_name)
+        tilCount = view.findViewById(R.id.til_count)
+
+        editName = view.findViewById(R.id.edit_text_name)
+
+        editName.addTextChangedListener {
+            tilName.error = null
+        }
+
+        editCount = view.findViewById(R.id.edit_text_count)
+        editCount.addTextChangedListener {
+            tilCount.error = null
+        }
+
+        buttonSave = view.findViewById(R.id.buttonSave)
+    }
+
+//    private fun lunchActivityForAdd(){
+//        buttonSave.setOnClickListener {
+//            viewModel.addItem(editName.text, editCount.text)
+//        }
+//    }
+//    private fun lunchActivityForEdit(){
+//        viewModel.getItem(itemId)
+//
+//        buttonSave.setOnClickListener {
+//            viewModel.editItem(editName.text, editCount.text)
+//        }
+//    }
+
+//    private var mode: String = MODE_UNDEF
+//    private var itemId: Long = ShopItem.UNDEFINED
+//    private fun parseIntent(){
+//        if (intent.hasExtra(EXTRA_MODE)){
+//            mode = intent.getStringExtra(EXTRA_MODE) ?: MODE_UNDEF
+//            Log.d(TAG, "parseIntent: mode = $mode")
+//            if (mode == MODE_ADD){
+//                lunchActivityForAdd()
+//            }
+//            else if (mode == MODE_EDIT && intent.hasExtra(EXTRA_ITEM_ID)){
+//                itemId = intent.getLongExtra(EXTRA_ITEM_ID, ShopItem.UNDEFINED)
+//                Log.d(TAG, "parseIntent: itemId = $itemId")
+//                lunchActivityForEdit()
+//            } else {
+//                throw IllegalArgumentException("Item Id is not defined")
+//            }
+//        } else {
+//            throw IllegalArgumentException("Mode is not defined")
+//        }
+//    }
+
+    private fun registerLiveData(){
+        viewModel.itemLiveData.observe(this){
+            editName.setText(it.name)
+            editCount.setText(it.count.toString())
+        }
+
+        viewModel.errorNameLD.observe(this){
+            tilName.error = if (it){
+                null
+            } else {
+                getString(R.string.name_error)
+            }
+        }
+        viewModel.errorCountLD.observe(this){
+            tilCount.error = if (it){
+                null
+            } else {
+                getString(R.string.count_error)
+            }
+        }
+
+        viewModel.finishActivityLD.observe(this){
+//            finish()
+//            onBackPressed()
+        }
     }
 
 }
