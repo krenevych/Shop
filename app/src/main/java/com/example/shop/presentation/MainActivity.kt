c0f3899ca09b610201b1d43de38bbbe50c8285f5
Andrii Krenevych <akrenevych@gmail.com>
2023-11-21 14:56:26 +0200

    Fragment - finish implementation

--- app/src/main/java/com/example/shop/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shop/presentation/MainActivity.kt
@@ -3,8 +3,8 @@ package com.example.shop.presentation
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
@@ -19,15 +19,37 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: ShopItemAdapter
     private lateinit var buttonAdd: FloatingActionButton
+    private var fragmentContainer: FragmentContainerView? = null
 
     var item: ShopItem?= null
+
+
+    private fun setupFragment(fragment: ShopItemFragment){
+        supportFragmentManager.popBackStack()
+        supportFragmentManager
+            .beginTransaction()
+            .addToBackStack(null)
+//            .add(R.id.shop_item_container, fragment)
+            .replace(R.id.shop_item_container, fragment)
+            .commit()
+    }
+
+    val isPortrait : Boolean
+        get() = fragmentContainer == null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fragmentContainer = findViewById(R.id.shop_item_container)
+
         buttonAdd = findViewById(R.id.buttonAdd)
         buttonAdd.setOnClickListener {
-            lunchActivityForAdd()
+            if (isPortrait){
+                lunchActivityForAdd()
+            } else {
+                setupFragment(ShopItemFragment.newInstanceFragmentAdd())
+            }
         }
 
         recyclerView = findViewById(R.id.shop_item_recycler_view)
@@ -35,7 +57,11 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
         adapter.clickListener = { view: View, item: ShopItem->
 //            Toast.makeText(this@MainActivity, item.toString(), Toast.LENGTH_SHORT).show()
-            lunchActivityForEdit(item.id)
+            if (isPortrait) {
+                lunchActivityForEdit(item.id)
+            } else {
+                setupFragment(ShopItemFragment.newInstanceFragmentEdit(item.id))
+            }
         }
 
         adapter.longClickListener = { view: View, item: ShopItem->
